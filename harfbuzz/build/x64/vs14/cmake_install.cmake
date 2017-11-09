73c0aa1b829872af660bff295bac2305daff909c
Chungrae Cho <ccr@emstone.com>
2017-11-09 14:27:44 +0900

    fix building x64 for harfbuzz

--- harfbuzz/build/x64/vs14/cmake_install.cmake
+++ harfbuzz/build/x64/vs14/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/gtk-build/build/Win32/harfbuzz
+# Install script for directory: C:/gtk-build/build/x64/harfbuzz
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,37 +29,37 @@ endif()
 
 if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/harfbuzz" TYPE FILE FILES
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-version.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-blob.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-buffer.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-common.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-deprecated.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-face.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-font.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-set.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-shape.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-shape-plan.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-unicode.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-ot.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-ot-font.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-ot-layout.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-ot-math.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-ot-shape.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-ot-tag.h"
-    "C:/gtk-build/build/Win32/harfbuzz/src/hb-ot-var.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-version.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-blob.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-buffer.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-common.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-deprecated.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-face.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-font.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-set.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-shape.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-shape-plan.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-unicode.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-ot.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-ot-font.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-ot-layout.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-ot-math.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-ot-shape.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-ot-tag.h"
+    "C:/gtk-build/build/x64/harfbuzz/src/hb-ot-var.h"
     )
 endif()
 
 if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/gtk-build/build/Win32/harfbuzz/build/x64/vs14/Debug/harfbuzz.lib")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/gtk-build/build/x64/harfbuzz/build/x64/vs14/Debug/harfbuzz.lib")
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/gtk-build/build/Win32/harfbuzz/build/x64/vs14/Release/harfbuzz.lib")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/gtk-build/build/x64/harfbuzz/build/x64/vs14/Release/harfbuzz.lib")
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/gtk-build/build/Win32/harfbuzz/build/x64/vs14/MinSizeRel/harfbuzz.lib")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/gtk-build/build/x64/harfbuzz/build/x64/vs14/MinSizeRel/harfbuzz.lib")
   elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
-    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/gtk-build/build/Win32/harfbuzz/build/x64/vs14/RelWithDebInfo/harfbuzz.lib")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/gtk-build/build/x64/harfbuzz/build/x64/vs14/RelWithDebInfo/harfbuzz.lib")
   endif()
 endif()
 
@@ -71,5 +71,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/gtk-build/build/Win32/harfbuzz/build/x64/vs14/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/gtk-build/build/x64/harfbuzz/build/x64/vs14/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
